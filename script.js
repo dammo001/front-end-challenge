413c8e2347bb37e13db805fdf3ff5b9db3ddf1ad
David Ammons <dammo001@gmail.com>
2015-12-10 16:58:02 -0800

    primary functionality present

--- script.js
+++ script.js
@@ -4,30 +4,42 @@ function domobj(){
   self.products   = [];
 
   self.getproducts = function(url){
+    var count = 0;
     $.getJSON(url, function(response){
         for(i=0; i<response.sales.length ; i++){
           self.products.push( new productobj(response.sales[i], i)  );
+          count += 1; 
+          if (count === response.sales.length){
+            self.updateproducthtml();
+          }
         }
     });
   }
     
   self.updateproducthtml = function(){
-    for( i=0; i< self.products.length ; i++){
-      self.products[i].updatehtml();
-    }
-    setTimeout(self.updatedom.bind(self),200)
+    $.get('product-template.html', function(template){
+      var counter = 0; 
+      for( i=0; i< self.products.length ; i++){
+        self.products[i].updatehtml(template);
+        counter += 1; 
+        if (counter === self.products.length){
+          self.updatedom(); 
+        }
+      }
+    });
   }
   
   self.updatedom = function(){
     var i=0
     thishtml='';
     for( i=0; i< self.products.length ; i++){
-      if (i % 3 == 0 ){  thishtml += "<div class='row'>"; console.log("START") }
+      if (i % 3 == 0 ){  thishtml += "<div class='row'>";}
       thishtml += self.products[i].htmlview;
-      if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";console.log("FINISH")}
+      if ((i % 3 == 2) || i == (self.products.length-1) ){thishtml += "</div>";}
     }
+    
     setTimeout(self.addxs.bind(self), 0); 
-    $("#content").append(thishtml)
+    $("#content").append(thishtml);
   }
 
   self.addxs = function(){
@@ -48,17 +60,14 @@ function productobj(product, i){
   self.index        = i
   self.custom_class = "col"+ ((i % 3) +1) 
   
-  self.updatehtml= function(){
-    $.get('product-template.html', function(template){
+  self.updatehtml= function(template){
       self.htmlview = template.replace('{image}', self.photo).replace('{title}', self.title).replace('{tagline}', self.tagline).replace('{url}', self.url).replace('{description}', self.description).replace('{custom_class}', self.custom_class);
-    });
   }
 }
 
-
 var page=new domobj();
 page.getproducts('./data.json');
-setTimeout(page.updateproducthtml.bind(page),20);
+
 
 
 
